ffb95e3e9f5736a5c19ea0b38bef1cb53558c3ee
zcxkkk <zcxkkk@163.com>
2014-12-24 17:24:16 +0800

    Update and rename daysRange.java to daysInterval.java
    change daysRange to daysInterval

--- static/js/tool/daysInterval.java
+++ static/js/tool/daysInterval.java
@@ -2,17 +2,17 @@
 	 * 日期间隔天数
 	 * @param startDateStr
 	 * @param endDateStr
-	 * @return (int)daysRange
+	 * @return (int)daysInterval
 	 */
-	public static int daysRange(String startDateStr, String endDateStr) {
+	public static int daysInterval(String startDateStr, String endDateStr) {
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-		long daysRange = 0;
+		long daysInterval = 0;
 		try { 
 			Date startDate = sdf.parse(startDateStr);
 			Date endDate = sdf.parse(endDateStr);
-			daysRange = (endDate.getTime() - startDate.getTime()) / (24 *60 *60 *1000); 
+			daysInterval = (endDate.getTime() - startDate.getTime()) / (24 *60 *60 *1000); 
 		} catch (Exception e) { 
 		    System.out.println(e.getMessage()); 
 		} 
-		return (int)daysRange;
+		return (int)daysInterval;
 	}
