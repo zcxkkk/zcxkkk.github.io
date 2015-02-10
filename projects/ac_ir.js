2b7fcd00b44f9ed367c34d7ac30a88676e05424c
zcxkkk <zcxkkk@163.com>
2015-02-10 09:19:34 +0800

    acfun ir js

---
+++ projects/ac_ir.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name	Acfun Unreplaceable
+// @author	googleGuard
+// @version	1.1
+// @include	http://www.acfun.tv/v/a*
+// @run-at	document-start
+// @grant	none
+// ==/UserScript==
+
+(function(f) {
+	document.documentElement.appendChild(document.createElement('script')).textContent = '(' + f + ')()'
+})(function(){
+	String.prototype.toStringT = String.prototype.toString;
+	String.prototype.toString = function() {
+		this == location.href && document.querySelector('#area-part-view a.btn.active').setAttribute('data-from', 'default');
+		return this.toStringT()
+	};
+});
